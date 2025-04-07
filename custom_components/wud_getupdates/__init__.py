7764aa78360dd8807332d8de889fc20b0972f2a9
pv90 <37786762+pveiga90@users.noreply.github.com>
2025-04-07 01:21:18 +0100

    Update __init__.py
    Issue #4

--- custom_components/wud_getupdates/__init__.py
+++ custom_components/wud_getupdates/__init__.py
@@ -1,17 +1,20 @@
+import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-DOMAIN = "wud_getupdates"
+from .const import DOMAIN
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    return True
+_LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Set up WUD Monitor from a config entry."""
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
     )
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    return True
+
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Unload a config entry."""
+    return await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
